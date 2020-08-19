7bd6c75e2478360f75f1d3f10c233df09641fd04
Miguel Faldutti <miguelfaldutti87@gmail.com>
2020-08-19 14:31:14 -0300

    Rutas Relativas Corregidas

--- main.py
+++ main.py
@@ -5,26 +5,20 @@ import shutil
 import paths
 
 def armadoBaseline():
-#    fichero = open(paths.db,"w")
-#    fichero.write("")
-#    fichero.close()
+    os.makedirs(paths.tmp)
     webModern = requests.get(paths.webModern)
     webPioneer = requests.get(paths.webPioneer)
     webStandard = requests.get(paths.webStandard)
     open(paths.tmpModern,"w").write(webModern.text)
     open(paths.tmpPioneer,"w").write(webPioneer.text)
     open(paths.tmpStandard,"w").write(webStandard.text)
-    descargaBD(paths.tmpModern,"modern")
-    descargaBD(paths.tmpPioneer,"pioneer")
-    descargaBD(paths.tmpStandard,"standard")
-
-#def grabarDatos(linea):
-#    fichero = open(paths.db,"a")
-#    fichero.write(linea +"\n")
-#    fichero.close()  
+    descargaBD(paths.tmpModern,"modern/")
+    descargaBD(paths.tmpPioneer,"pioneer/")
+    descargaBD(paths.tmpStandard,"standard/")
 
 def descargaBD(file, tipo):
-    path = paths.tmp + tipo + "/"
+    os.makedirs(paths.tmp + tipo)
+    path = paths.tmp + tipo
     with open(file,"r") as fl:
         for line in fl:
             inicio = line.find('<a href="/archetype/')
@@ -36,30 +30,32 @@ def descargaBD(file, tipo):
                     f.write(deck.text)
 
 def descargaDecks(tipo):
-    filelist = os.listdir(paths.tmp + tipo + "/")
-    shutil.rmtree(paths.deck+ tipo + "/")
-    os.makedirs(paths.deck + tipo + "/")
+    filelist = os.listdir(paths.tmp + tipo)
+    shutil.rmtree(paths.deck + tipo)
+    os.makedirs(paths.deck + tipo)
     for file in filelist:
-        with open(os.path.join(paths.tmp + tipo + "/",file),"r",encoding='utf-8') as file2:
+        with open(os.path.join(paths.tmp + tipo, file),"r",encoding='utf-8') as file2:
             for line in file2:
                 vD = line.find("/deck/download/")
                 vD2 = line.find('">')
                 if (vD != -1) and (vD2 != -1):
                     deck2 = line[vD:vD2]
                     pagina2 = paths.mainWeb + deck2
-                    wget.download(pagina2, out=paths.deck + tipo + "/")
-             
+                    wget.download(pagina2, out=paths.deck + tipo)
+                     
+                 
 if __name__ == '__main__':
     print("Iniciando Sistema de Chequeo de Decks")
     print("Sincronizando datos...")
     armadoBaseline()
     print("Sincronizacion Completada")
     print("Comenzando descarga de Decks...")
-    descargaDecks("modern")
+    descargaDecks("modern/")
     print("Descarga de Decks de Modern Completa")
-    descargaDecks("pioneer")
+    descargaDecks("pioneer/")
     print("Descarga de Decks de Pioneer Completa")
-    descargaDecks("standard")
+    descargaDecks("standard/")
     print("Descarga de Decks de Standard Completa")
+    shutil.rmtree(paths.tmp)
     print("Descarga de Decks Finalizada")
     
--- paths.py
+++ paths.py
@@ -1,16 +1,10 @@
-'''
-Created on 11-ago-2020
-
-@author: PC-Escritorio
-'''
 
 mainWeb = "https://www.mtggoldfish.com/"
 webModern = "https://www.mtggoldfish.com/metagame/modern/full#paper"
 webPioneer = "https://www.mtggoldfish.com/metagame/pioneer/full#paper"
 webStandard = "https://www.mtggoldfish.com/metagame/standard/full#paper"
-tmp = "D:/Scripts/MagicDeckCheck/tmp/"
-tmpModern = "D:/Scripts/MagicDeckCheck/tmp/modern.txt"
-tmpPioneer = "D:/Scripts/MagicDeckCheck/tmp/pioneer.txt"
-tmpStandard = "D:/Scripts/MagicDeckCheck/tmp/standard.txt"
-deck = "D:/Scripts/MagicDeckCheck/decks/"
-#db = "D:/Scripts/MagicDeckCheck/tmp/db.txt"
+tmp = "./tmp/"
+tmpModern = "./tmp/modern.txt"
+tmpPioneer = "./tmp/pioneer.txt"
+tmpStandard = "./tmp/standard.txt"
+deck = "./decks/"
